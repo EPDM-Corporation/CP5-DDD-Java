2d266ccde9b502907320040b54dd87c8646de37c
Kayque <kayquecarvalho020@gmail.com>
2026-03-16 15:43:10 -0300

    Feat: Criando projeto em SpringBoot

---
+++ src/main/java/br/com/fiap2espa/CpJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap2espa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap2espa/CpJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap2espa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
