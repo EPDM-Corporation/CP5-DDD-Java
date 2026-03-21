702fbddc83e70185e71281493366055dff8f785b
Kayque <kayquecarvalho020@gmail.com>
2026-03-20 22:42:06 -0300

    feat: Implementando repositórios JPA para entidades de domínio

--- src/main/java/br/com/fiap2espa/cp4/domain/repository/ProspectanteRepository.java
+++ src/main/java/br/com/fiap2espa/cp4/domain/repository/ProspectanteRepository.java
@@ -1,9 +1,9 @@
 package br.com.fiap2espa.cp4.domain.repository;
 
-import br.com.fiap2espa.cp4.domain.model.Vendedor;
+import br.com.fiap2espa.cp4.domain.model.Prospectante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VendedorRepository extends JpaRepository<Vendedor,Long> {
+public interface ProspectanteRepository extends JpaRepository<Prospectante, Long> {
 }
